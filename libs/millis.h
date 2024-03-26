7716c4ad63e8655258d3c1dd61dc6dfa61366502
linuxseller <shibanutyitokom@yandex.ru>
2024-03-26 20:34:11 +0500

    update Nox

--- libs/millis.h
+++ libs/millis.h
@@ -1,29 +1,47 @@
 #include <avr/interrupt.h>
+#define MICROSECONDS_PER_TIMER0_OVERFLOW ((64 * 256)/(F_CPU/1000000))
+#define MILLIS_INC (MICROSECONDS_PER_TIMER0_OVERFLOW / 1000)
+// the fractional number of milliseconds per timer0 overflow. we shift right
+// by three to fit these numbers into a byte. (for the clock speeds we care
+// about - 8 and 16 MHz - this doesn't lose precision.)
+#define FRACT_INC 3//128//((MICROSECONDS_PER_TIMER0_OVERFLOW % 1000) >> 3)
+#define FRACT_MAX (1000 >> 3)
 volatile unsigned long timer0_millis = 0;
-ISR(TIMER0_COMPA_vect)
-{
-  timer0_millis++;
+volatile unsigned long timer0_fract = 0;
+ISR(TIMER0_COMPA_vect){
+    timer0_millis++;
 }
 // When TCNT0 == OCR0A, e comparator signals a match. A match will set the Output Compare Flag (OCF0A or OCF0B) at the next timer clock cycle.
 // The output compare flag is automatically cleared when the interrupt is executed
 void init_millis(){
   unsigned long ctc_match_overflow;
-  ctc_match_overflow = ((F_CPU / 1000) / 8); //when timer1 is this value, 1ms has passed
-  // (Set timer to clear when matching ctc_match_overflow) | (Set clock divisor to 8)
-  // WGM0[2:0] = 0x2
-  TCCR0A = (1 << COM0A1) | (1 << WGM01);
-  TCCR0B = (1 << CS01); // selecting clock source
+  ctc_match_overflow = (F_CPU / 1000)/64; //when timer1 is this value, 1ms has passed
+  TCCR0A = (1 << WGM01);
+  TCCR0B = (1 << CS01) | (1 << CS00); // selecting clock source
   OCR0A = ctc_match_overflow;
   // Enable the compare match interrupt
   TIMSK0 |= (1 << OCIE0A);
   sei();
 }
+unsigned long millis()
+{
+	unsigned long m;
+	uint8_t oldSREG = SREG;
 
-unsigned long millis(){
-  unsigned long millis_return;
-  uint8_t oldSREG = SREG;
-  cli();
-  millis_return = timer0_millis;
-  SREG = oldSREG;
-  return millis_return;
+	// disable interrupts while we read timer0_millis or we might get an
+	// inconsistent value (e.g. in the middle of a write to timer0_millis)
+	cli();
+	m = timer0_millis;
+	SREG = oldSREG;
+
+	return m;
 }
+/* unsigned long millis(){ */
+/*   unsigned long millis_return; */
+/*   uint8_t oldSREG = SREG; */
+/*   cli(); */
+/*   millis_return = timer0_millis; */
+/*   SREG = oldSREG; */
+/*   sei(); */
+/*   return millis_return; */
+/* } */
--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <stdint.h>
 #include <avr/cpufunc.h>
 
+#include "data.h"
 #include "libs/millis.h"
 #define ABS(a) (a>0)?a:(-(a))
 #define WIDTH 16
@@ -13,23 +14,35 @@
 #define COL 0x10
 #define CENTER 8
 #define RADIUS 7
-
+#define CLOCK_UPDATE_MS 1*1000 // every 1 second
+#define UPDATE_ACCORDING_TO_MOD() { \
+    switch(cur_mode) { \
+        case DIGITAL_CLOCK: \
+            updateDigitalClock(); \
+            break; \
+        case ANALOG_CLOCK: \
+            updateAnalogClock(); \
+            break; \
+        default: break; \
+    } \
+}
 typedef struct {
     int8_t x, y;
 } Vector2;
 
 struct cRGB led[MAXPIX];
 int8_t display[MAXPIX];
-int8_t hours=0, minutes=-5;
-
+int8_t hours=0, minutes=0;
+enum CurrentMode {ANALOG_CLOCK, DIGITAL_CLOCK, MODES_COUNT};
 Vector2 hours_pos, minutes_pos;
 void my_memset(void *p, int8_t c, uint16_t n){
     while(n--){
         ((int8_t*)p)[n]=c;
     }
 }
-
 void setPX(int8_t x, int8_t y, uint32_t col){
+    x=WIDTH-x;
+    y=HEIGHT-y;
     if(x%2!=0){
         y=WIDTH-1-y;
     }
@@ -37,51 +50,13 @@ void setPX(int8_t x, int8_t y, uint32_t col){
     led[x*WIDTH+y].g=(col>>8)&0xff;
     led[x*WIDTH+y].b=(col>>0)&0xff;
 }
-
-uint32_t statecolor[]={
-    [0] = 0x00000000,
-    [1] = 0x00101010,
-    [2] = 0x00000020,
-    [3] = 0x00200000,
-};
-
-int8_t lookup[][2]  ={
-    [0] = {8,2},
-    [1] = {11,2},
-    [2] = {13,5},
-    [3] = {14,8},
-    [4] = {13,11},
-    [5] = {11,13},
-    [6] = {8,14},
-    [7] = {4,13},
-    [8] = {2,11},
-    [9] = {2,8},
-    [10] = {2,5},
-    [11] = {5,2}
-};
-
-int8_t lookup_h[][2] = {
-    [0] = {8,4},
-    [1] = {10,4},
-    [2] = {11,6},
-    [3] = {12,8},
-    [4] = {11,10},
-    [5] = {9,11},
-    [6] = {7,12},
-    [7] = {5,11},
-    [8] = {4,10},
-    [9] = {4,8},
-    [10] = {4,5},
-    [11] = {6,4}
-};
-
 int8_t tocenter(int8_t a)
 {
     if(a>8){return a-1;}
     return a+1;
 }
 
-void drawClock(void)
+void drawScreen(void)
 {
     for(int8_t x=0; x<16; x++){
         for(int8_t y=0; y<16; y++){
@@ -90,7 +65,32 @@ void drawClock(void)
     }
     ws2812_sendarray((uint8_t *)led,MAXPIX*3);
 }
-void plotLine(int8_t x0, int8_t y0, int8_t x1, int8_t y1, int8_t c)
+void drawNumber(uint8_t id, uint8_t pos){
+    uint8_t dx = 7*(pos%2==0);
+    uint8_t dy = 8*(pos>2);
+    if(pos==1 || pos==2){
+        dy+=1;
+    }
+    for(uint8_t y=0; y<7; y++){
+        for(int8_t bit=7; bit>=0; bit--){
+            if((letters[id][y]>>bit)&1){
+                display[(8-bit+dx)+(y+1+dy)*WIDTH] = YELLOW;
+            } else {
+                display[(8-bit+dx)+(y+1+dy)*WIDTH] = BLACK;
+            }
+        }
+    }
+}
+
+void updateDigitalClock(void){
+    my_memset(display, 0, 16*16*sizeof(*display));
+    drawNumber(hours/10,1);
+    drawNumber(hours%10,2);
+    drawNumber(minutes/10,3);
+    drawNumber(minutes%10,4);
+}
+
+void plotLine(int8_t x0, int8_t y0, int8_t x1, int8_t y1, enum ColorEnum c)
 {
     int8_t dx = ABS(x1 - x0);
     int8_t sx = x0 < x1 ? 1 : -1;
@@ -114,12 +114,12 @@ void plotLine(int8_t x0, int8_t y0, int8_t x1, int8_t y1, int8_t c)
     }
 }
 
-void plotArrow(int8_t x1, int8_t y1, int8_t c)
+void plotArrow(int8_t x1, int8_t y1, enum ColorEnum c)
 {
     plotLine(CENTER,CENTER,x1,y1, c);
 }
 
-void updateScreen(void)
+void updateAnalogClock(void)
 {
     my_memset(display, 0, 16*16*sizeof(*display));
     for(int8_t x=0; x<16; x++){
@@ -128,64 +128,114 @@ void updateScreen(void)
             int8_t yy = y*2/2;
             display[xx+yy*16] = (xx-CENTER)*(xx-CENTER)+(yy-CENTER)*(yy-CENTER)<=RADIUS*RADIUS;
             if((xx-CENTER)*(xx-CENTER)+(yy-CENTER)*(yy-CENTER)<=(RADIUS-1)*(RADIUS-1)){
-                display[xx+yy*16] = 0;
+                display[xx+yy*16] = BLACK;
             }
         }
     }
-    plotArrow(lookup[minutes/5][0], lookup[minutes/5][1], 3);
-    plotArrow(lookup_h[hours][0], lookup_h[hours][1], 2);
+    plotArrow(lookup[minutes/5][0], lookup[minutes/5][1], RED);
+    plotArrow(lookup_h[hours][0], lookup_h[hours][1], BLUE);
 }
 
-void updateClock(void)
+void updateTime(void)
 {
-    minutes += 5;
+    minutes += 1;
     hours   += minutes==60;
     hours   %= 12;
     minutes %= 60;
-    updateScreen();
 }
-
+enum CurrentMode cur_mode = DIGITAL_CLOCK;
 int main(void){
     DDRB = (1<<DDB5);
     DDRB &= ~(1<<PINB4);
     DDRB &= ~(1<<PINB2);
-    PORTB |= (1<<PINB4) | (1<<PINB2);
+    DDRB &= ~(1<<PINB3);
+    PORTB |= (1<<PINB4) | (1<<PINB3) | (1<<PINB2);
     init_millis();
     _NOP();
-    unsigned long prev=0;
+    unsigned long clock_prev_timer=0;
+    unsigned long draw_clock_prev_timer=0;
     unsigned long hours_btn_timer=0;
     unsigned long minutes_btn_timer=0;
-    updateClock();
-    drawClock();
+    unsigned long modes_change_btn_timer=0;
+    switch(cur_mode){
+        case ANALOG_CLOCK:
+            updateAnalogClock();
+            break;
+        case DIGITAL_CLOCK:
+            updateDigitalClock();
+            break;
+        default:break;
+    };
+    drawScreen();
+    /* for(int i=0;;){ */
+    /*     drawNumber(i, 1); */
+    /*     drawNumber((i+1)%10, 2); */
+    /*     drawNumber((i+2)%10, 3); */
+    /*     drawNumber((i+3)%10, 4); */
+    /*     drawScreen(); */
+    /*     i+=1; */
+    /*     i%=10; */
+    /*     _delay_ms(1000); */
+    /* } */
+    /* return 0; */
 	while(1){
         _NOP();
-        unsigned long int millis_now = millis();
-        uint8_t pinb = PINB;
+        /* uint8_t pinb = PINB; */
+        uint8_t pinb =0xff;
+        if(!((pinb>>PINB3)&1)){
+            minutes=0;
+            hours=0;
+            continue;
+            if(millis()-modes_change_btn_timer>250*8){
+                modes_change_btn_timer = millis();
+                cur_mode += 1;
+                cur_mode %= MODES_COUNT;
+                UPDATE_ACCORDING_TO_MOD();
+                drawScreen();
+            }
+        }
         if(!((pinb>>PINB4)&1)){
-            if(millis_now-hours_btn_timer>5000){
-                hours_btn_timer = millis_now;
+            if(millis()-hours_btn_timer>250*8){
+                hours_btn_timer = millis();
                 hours++;
                 hours%=12;
-                updateScreen();
-                drawClock();
             }
+            UPDATE_ACCORDING_TO_MOD();
+            drawScreen();
+            clock_prev_timer=millis();
             continue;
         }
         if(!((pinb>>PINB2)&1)){
-            if(millis_now-minutes_btn_timer>5000){
-                minutes_btn_timer = millis_now;
-                minutes+=5;
+            if(millis()-minutes_btn_timer>250*8){
+                minutes_btn_timer = millis();
+                if(cur_mode==ANALOG_CLOCK){
+                    minutes+=5;
+                } else if(cur_mode==DIGITAL_CLOCK) {
+                    minutes+=1;
+                }
                 minutes%=60;
-                updateScreen();
-                drawClock();
             }
+            UPDATE_ACCORDING_TO_MOD();
+            drawScreen();
+            clock_prev_timer=millis();
             continue;
         }
-#define CLOCK_UPDATE_MS 5*60*1000*10
-        if(millis_now-prev>(long unsigned int)CLOCK_UPDATE_MS){
-            prev=millis();
-            updateClock();
-            drawClock();
+        if(millis()-draw_clock_prev_timer>(long unsigned int)CLOCK_UPDATE_MS){
+            draw_clock_prev_timer = millis();
+            switch(cur_mode){
+                case ANALOG_CLOCK:
+                    updateAnalogClock();
+                    break;
+                case DIGITAL_CLOCK:
+                    updateDigitalClock();
+                    break;
+                default:break;
+            }
+            drawScreen();
+        }
+        if(millis()-clock_prev_timer>(long unsigned int)CLOCK_UPDATE_MS){
+            clock_prev_timer=millis();
+            updateTime();
         }
     }
 }
