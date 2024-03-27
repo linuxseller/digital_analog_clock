1cadd879760a0a2db2e2a4655bd64b9aabba1675
linuxseller <shibanutyitokom@yandex.ru>
2024-03-27 20:56:59 +0500

    sorry, forgot what I changed :3

--- libs/millis.h
+++ libs/millis.h
@@ -1,11 +1,4 @@
 #include <avr/interrupt.h>
-#define MICROSECONDS_PER_TIMER0_OVERFLOW ((64 * 256)/(F_CPU/1000000))
-#define MILLIS_INC (MICROSECONDS_PER_TIMER0_OVERFLOW / 1000)
-// the fractional number of milliseconds per timer0 overflow. we shift right
-// by three to fit these numbers into a byte. (for the clock speeds we care
-// about - 8 and 16 MHz - this doesn't lose precision.)
-#define FRACT_INC 3//128//((MICROSECONDS_PER_TIMER0_OVERFLOW % 1000) >> 3)
-#define FRACT_MAX (1000 >> 3)
 volatile unsigned long timer0_millis = 0;
 volatile unsigned long timer0_fract = 0;
 ISR(TIMER0_COMPA_vect){
@@ -13,35 +6,24 @@ ISR(TIMER0_COMPA_vect){
 }
 // When TCNT0 == OCR0A, e comparator signals a match. A match will set the Output Compare Flag (OCF0A or OCF0B) at the next timer clock cycle.
 // The output compare flag is automatically cleared when the interrupt is executed
-void init_millis(){
-  unsigned long ctc_match_overflow;
-  ctc_match_overflow = (F_CPU / 1000)/64; //when timer1 is this value, 1ms has passed
-  TCCR0A = (1 << WGM01);
-  TCCR0B = (1 << CS01) | (1 << CS00); // selecting clock source
-  OCR0A = ctc_match_overflow;
-  // Enable the compare match interrupt
-  TIMSK0 |= (1 << OCIE0A);
-  sei();
+void init_millis()
+{
+    unsigned long ctc_match_overflow;
+    ctc_match_overflow = (F_CPU / 1000)/64; //when TC0 is this value, 1ms has passed
+    TCCR0A = (1 << WGM01);
+    TCCR0B = (1 << CS01) | (1 << CS00); // set prescaler value (64 here)
+    // OCR0A = ctc_match_overflow;
+    *(uint8_t*)0x44 = ctc_match_overflow;
+    TIMSK0 |= (1 << OCIE0A);
+    sei();
 }
 unsigned long millis()
 {
 	unsigned long m;
 	uint8_t oldSREG = SREG;
-
-	// disable interrupts while we read timer0_millis or we might get an
-	// inconsistent value (e.g. in the middle of a write to timer0_millis)
 	cli();
 	m = timer0_millis;
 	SREG = oldSREG;
 
 	return m;
 }
-/* unsigned long millis(){ */
-/*   unsigned long millis_return; */
-/*   uint8_t oldSREG = SREG; */
-/*   cli(); */
-/*   millis_return = timer0_millis; */
-/*   SREG = oldSREG; */
-/*   sei(); */
-/*   return millis_return; */
-/* } */
--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-#include <util/delay.h>
 #include <avr/interrupt.h>
 #include <avr/io.h>
 #include "libs/light_ws2812.h"
@@ -7,6 +6,7 @@
 
 #include "data.h"
 #include "libs/millis.h"
+
 #define ABS(a) (a>0)?a:(-(a))
 #define WIDTH 16
 #define HEIGHT 16
@@ -14,7 +14,7 @@
 #define COL 0x10
 #define CENTER 8
 #define RADIUS 7
-#define CLOCK_UPDATE_MS 1*60*1000 // every 1 second
+#define CLOCK_UPDATE_MS 59583 // every 1 minute. clock generator is broken, so 1 minute does not pass every 60_000 miliseconds
 #define UPDATE_ACCORDING_TO_MOD() { \
     switch(cur_mode) { \
         case DIGITAL_CLOCK: \
@@ -26,21 +26,39 @@
         default: break; \
     } \
 }
+
 typedef struct {
     int8_t x, y;
 } Vector2;
 
+int8_t hours=0, minutes=0;
 struct cRGB led[MAXPIX];
 int8_t display[MAXPIX];
-int8_t hours=0, minutes=0;
-enum CurrentMode {ANALOG_CLOCK, DIGITAL_CLOCK, MODES_COUNT};
+uint16_t matrix_random=0xa5e6;
+enum CurrentMode {ANALOG_CLOCK, DIGITAL_CLOCK, MODE_DVD, MODE_MATRIX, MODES_COUNT};
 Vector2 hours_pos, minutes_pos;
-void my_memset(void *p, int8_t c, uint16_t n){
+
+struct {
+    Vector2 pos;
+    Vector2 speed;
+    Vector2 size;
+} dvd_logo = {.pos=(Vector2){5,4}, .speed=(Vector2){2,1}, .size=(Vector2){9,3}};
+
+uint16_t my_random(){
+    unsigned long rndseed = millis();
+    matrix_random = (rndseed&0x3f)|((rndseed&0x3f)<<5)|((rndseed&0x3f)<<10);
+    return matrix_random;
+}
+
+void my_memset(void *p, int8_t c, uint16_t n)
+{
     while(n--){
         ((int8_t*)p)[n]=c;
     }
 }
-void setPX(int8_t x, int8_t y, uint32_t col){
+
+void setPX(int8_t x, int8_t y, uint32_t col)
+{
     x=WIDTH-x;
     y=HEIGHT-y;
     if(x%2!=0){
@@ -49,11 +67,6 @@ void setPX(int8_t x, int8_t y, uint32_t col){
     led[x*WIDTH+y].r=(col>>16)&0xff;
     led[x*WIDTH+y].g=(col>>8)&0xff;
     led[x*WIDTH+y].b=(col>>0)&0xff;
-}
-int8_t tocenter(int8_t a)
-{
-    if(a>8){return a-1;}
-    return a+1;
 }
 
 void drawScreen(void)
@@ -65,7 +78,9 @@ void drawScreen(void)
     }
     ws2812_sendarray((uint8_t *)led,MAXPIX*3);
 }
-void drawNumber(uint8_t id, uint8_t pos){
+
+void drawNumber(uint8_t id, uint8_t pos)
+{
     uint8_t dx = 7*(pos%2==0);
     uint8_t dy = 8*(pos>2);
     if(pos==1 || pos==2){
@@ -82,7 +97,8 @@ void drawNumber(uint8_t id, uint8_t pos){
     }
 }
 
-void updateDigitalClock(void){
+void updateDigitalClock(void)
+{
     my_memset(display, 0, 16*16*sizeof(*display));
     drawNumber(hours/10,1);
     drawNumber(hours%10,2);
@@ -114,11 +130,68 @@ void plotLine(int8_t x0, int8_t y0, int8_t x1, int8_t y1, enum ColorEnum c)
     }
 }
 
+void drawRect(Vector2 lu, Vector2 sz, enum ColorEnum c)
+{
+    sz.x--;
+    sz.y--;
+    plotLine(lu.x,      lu.y,      lu.x+sz.x, lu.y, c);
+    plotLine(lu.x+sz.x, lu.y,      lu.x+sz.x, lu.y+sz.y, c);
+    plotLine(lu.x,      lu.y,      lu.x,      lu.y+sz.y, c);
+    plotLine(lu.x,      lu.y+sz.y, lu.x+sz.x, lu.y+sz.y, c);
+}
+
 void plotArrow(int8_t x1, int8_t y1, enum ColorEnum c)
 {
     plotLine(CENTER,CENTER,x1,y1, c);
 }
-
+Vector2 vec2Add(Vector2 a, Vector2 b){
+    return (Vector2){a.x+b.x, a.y+b.y};
+}
+void updateMatrix(void)
+{
+    uint16_t rnd = my_random();
+    for (int y = 16; y > 0; y--) {
+        for (int x = 0; x <WIDTH; x++) {
+            display[x+(y+1)*WIDTH] = display[x+y*WIDTH];
+        }
+    }
+    for(int bit=0; bit<16; bit++){
+        display[bit+WIDTH]=((rnd >> bit)&1)?GREEN:BLACK;
+    }
+}
+void updateDvd(void)
+{
+    my_memset(display, 0, 16*16*sizeof(*display));
+    if(dvd_logo.pos.x+dvd_logo.size.x+dvd_logo.size.x>WIDTH || dvd_logo.pos.x+dvd_logo.speed.x<0){
+        dvd_logo.speed.x*=-1;
+    }
+    if(dvd_logo.pos.y+dvd_logo.speed.y+dvd_logo.size.y>HEIGHT || dvd_logo.pos.y+dvd_logo.speed.y<1){
+        dvd_logo.speed.y*=-1;
+    }
+    dvd_logo.pos = vec2Add(dvd_logo.pos, dvd_logo.speed);
+    //## # ###
+    //# ## ## #
+    //##  # ##
+    uint8_t y = dvd_logo.pos.y;
+    uint8_t x = dvd_logo.pos.x;
+    display[(0+x)+(0+y)*WIDTH]=BLUE;
+    display[(0+x)+(1+y)*WIDTH]=BLUE;
+    display[(0+x)+(2+y)*WIDTH]=BLUE;
+    display[(1+x)+(0+y)*WIDTH]=BLUE;
+    display[(1+x)+(2+y)*WIDTH]=BLUE;
+    display[(2+x)+(1+y)*WIDTH]=BLUE;
+    display[(3+x)+(0+y)*WIDTH]=BLUE;
+    display[(3+x)+(1+y)*WIDTH]=BLUE;
+    display[(4+x)+(2+y)*WIDTH]=BLUE;
+    display[(5+x)+(0+y)*WIDTH]=BLUE;
+    display[(5+x)+(1+y)*WIDTH]=BLUE;
+    display[(6+x)+(0+y)*WIDTH]=BLUE;
+    display[(6+x)+(1+y)*WIDTH]=BLUE;
+    display[(6+x)+(2+y)*WIDTH]=BLUE;
+    display[(7+x)+(0+y)*WIDTH]=BLUE;
+    display[(7+x)+(2+y)*WIDTH]=BLUE;
+    display[(8+x)+(1+y)*WIDTH]=BLUE;
+}
 void updateAnalogClock(void)
 {
     my_memset(display, 0, 16*16*sizeof(*display));
@@ -157,6 +230,8 @@ int main(void){
     unsigned long hours_btn_timer=0;
     unsigned long minutes_btn_timer=0;
     unsigned long modes_change_btn_timer=0;
+    unsigned long game_timer=0;
+    unsigned long matrix_timer=0;
     switch(cur_mode){
         case ANALOG_CLOCK:
             updateAnalogClock();
@@ -167,21 +242,13 @@ int main(void){
         default:break;
     };
     drawScreen();
-    /* for(int i=0;;){ */
-    /*     drawNumber(i, 1); */
-    /*     drawNumber((i+1)%10, 2); */
-    /*     drawNumber((i+2)%10, 3); */
-    /*     drawNumber((i+3)%10, 4); */
-    /*     drawScreen(); */
-    /*     i+=1; */
-    /*     i%=10; */
-    /*     _delay_ms(1000); */
-    /* } */
-    /* return 0; */
 	while(1){
+        if(millis()-clock_prev_timer>(long unsigned int)CLOCK_UPDATE_MS){
+            clock_prev_timer=millis();
+            updateTime();
+        }
         _NOP();
         uint8_t pinb = PINB;
-        /* uint8_t pinb =0xff; */
         if(!((pinb>>PINB3)&1)){
             if(millis()-modes_change_btn_timer>250){
                 modes_change_btn_timer = millis();
@@ -217,22 +284,24 @@ int main(void){
             clock_prev_timer=millis();
             continue;
         }
+        if(cur_mode==MODE_DVD){
+            if(millis()-game_timer>100){
+                updateDvd();
+                drawScreen();
+                game_timer = millis();
+            }
+        }
+        if(cur_mode==MODE_MATRIX){
+            if(millis()-matrix_timer>250){
+                updateMatrix();
+                drawScreen();
+                matrix_timer = millis();
+            }
+        }
         if(millis()-draw_clock_prev_timer>(long unsigned int)CLOCK_UPDATE_MS){
             draw_clock_prev_timer = millis();
-            switch(cur_mode){
-                case ANALOG_CLOCK:
-                    updateAnalogClock();
-                    break;
-                case DIGITAL_CLOCK:
-                    updateDigitalClock();
-                    break;
-                default:break;
-            }
+            UPDATE_ACCORDING_TO_MOD();
             drawScreen();
-        }
-        if(millis()-clock_prev_timer>(long unsigned int)CLOCK_UPDATE_MS){
-            clock_prev_timer=millis();
-            updateTime();
         }
     }
 }
